4fccd59ccb8014a153e081d69673bd850fd91350
Lê Văn Hậu <levanhau@MacBook-Pro-cua-Hau.local>
2026-02-28 16:19:26 +0700

    update models ThongBao and ThongBaoNguoiDung

--- app/Models/Interaction/ThongBao.php
+++ app/Models/Interaction/ThongBao.php
@@ -3,8 +3,33 @@
 namespace App\Models\Interaction;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Auth\TaiKhoan;
 
 class ThongBao extends Model
 {
     //
+    protected $table = 'thongbao';
+    protected $primaryKey = 'thongBaoId';
+    protected $fillable = [
+        'tieuDe',
+        'noiDung',
+        'nguoiGuiId',
+        'loaiThongBao',
+        'doiTuongGui', // 	0-Tất cả, 1-Theo lớp, 2-Theo khóa học, 3-Cá nhân,....
+        'doiTuongId',
+        'ngayGui',
+        'trangThai',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function nguoiGui()
+    {
+        return $this->belongsTo(TaiKhoan::class, 'nguoiGuiId', 'taiKhoanId');
+    }
+
+    public function doiTuong()
+    {
+        return $this->belongsTo(TaiKhoan::class, 'doiTuongId', 'taiKhoanId');
+    }
 }
--- app/Models/Interaction/ThongBaoNguoiDung.php
+++ app/Models/Interaction/ThongBaoNguoiDung.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class ThongBaoNguoiDung extends Model
 {
     //
+    protected $table = 'thongbaonguoidung';
+    protected $primaryKey = 'thongBaoNguoiDungId';
+    protected $fillable = [
+        'thongBaoId',
+        'taiKhoanId',
+        'daDoc',
+        'ngayDoc',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function thongBao()
+    {
+        return $this->belongsTo(ThongBao::class, 'thongBaoId', 'thongBaoId');
+    }
+
+    public function nguoiDung()
+    {
+        return $this->belongsTo(TaiKhoan::class, 'nguoiDungId', 'taiKhoanId');
+    }
 }
